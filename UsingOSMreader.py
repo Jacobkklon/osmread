4ac1916e8cc812c2a683a5cbdc1cffd495fab916
Jacobkklon <55518266+Jacobkklon@users.noreply.github.com>
2020-02-27 00:09:10 -0500

    Added code that plays with what's in the file

---
+++ UsingOSMreader.py
@@ -0,0 +1,8 @@
+from osmread import parse_file, Way
+
+highway_count = 0
+for entity in parse_file('Rutgers Pathways.osm'):
+    if isinstance(entity, Way) and 'highway' in entity.tags:
+        highway_count += 1
+
+print("%d highways found" % highway_count)
